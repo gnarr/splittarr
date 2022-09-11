9b773fd5da7735a9fd2154cb2c49b5c826a2e7de
Gunnar Cortes Heimisson <gnarr.c@gmail.com>
2022-09-11 16:19:23 +0200

    feat(config): add shnsplit, env parsing and default for shnsplit

--- src/settings.rs
+++ src/settings.rs
@@ -1,26 +1,44 @@
-use config::{Config, ConfigError, File};
-use serde_derive::Deserialize;
+pub mod settings {
+    use crate::globals::dirs;
+    use config::{Config, Environment, File};
+    use serde::Deserialize;
 
-#[derive(Debug, Deserialize)]
-#[allow(unused)]
-struct Lidarr {
-    url: String,
-    api_key: String,
-}
+    #[derive(Debug, Deserialize)]
+    #[allow(unused)]
+    pub struct Lidarr {
+        pub url: String,
+        pub api_key: String,
+    }
 
-#[derive(Debug, Deserialize)]
-#[allow(unused)]
-pub struct Settings {
-    debug: bool,
-    lidarr: Lidarr,
-}
+    #[derive(Debug, Deserialize)]
+    #[allow(unused)]
+    pub struct Shnsplit {
+        pub path: String,
+        pub overwrite: bool,
+    }
 
-impl Settings {
-    pub fn new() -> Result<Self, ConfigError> {
-        let config = Config::builder()
-            .add_source(File::with_name("config.toml"))
-            .build()?;
+    #[derive(Debug, Deserialize)]
+    #[allow(unused)]
+    pub struct Settings {
+        debug: bool,
+        lidarr: Lidarr,
+        shnsplit: Shnsplit,
+    }
 
-        config.try_deserialize()
+    impl Settings {
+        // pub fn new() -> Result<Self, ConfigError> {
+        pub fn new() -> Config {
+            let dirs = dirs();
+            let config_dir = dirs.config_dir();
+            let config_file = config_dir.join("config.toml");
+            let config = Config::builder()
+                .add_source(File::with_name(config_file.to_str().unwrap()))
+                .add_source(Environment::with_prefix("splittarr"))
+                .set_default("shnsplit.path", "shnsplit")
+                .unwrap()
+                .build()
+                .unwrap();
+            config
+        }
     }
 }
