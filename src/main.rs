b2cc9637d6144eff71f0e4c02e63ac74065c40ea
Gunnar Cortes Heimisson <gnarr.c@gmail.com>
2022-09-11 16:26:00 +0200

    feat(splittarr): featch lidarr download queue, save to database and send to shnsplit

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,47 @@
 mod settings;
 
-use settings::Settings;
+mod globals;
+mod lidarr;
+mod shnsplit;
+mod store;
 
-fn main() {
+use crate::settings::settings::Lidarr;
+use crate::shnsplit::shnsplit::split;
+use crate::store::Download;
+use exitfailure::ExitFailure;
+use lidarr::Queue;
+use std::collections::HashSet;
+use walkdir::WalkDir;
+
+#[tokio::main]
+async fn main() -> Result<(), ExitFailure> {
     println!("Splittarr");
 
-    let settings = Settings::new();
-    println!("{:?}", settings);
+    let queue = Queue::get().await?;
+    let mut paths = HashSet::new();
+    for record in queue.records {
+        if record.status == "completed" && record.tracked_download_state == "importFailed" {
+            let download = Download {
+                id: record.id,
+                title: record.title,
+                status: record.status,
+                output_path: record.output_path,
+                download_id: record.download_id,
+                tracked_download_state: record.tracked_download_state,
+            };
+            download.save().await?;
+            paths.insert(download.output_path);
+        }
+    }
+
+    for path in paths {
+        for file in WalkDir::new(path).into_iter().filter_map(|file| file.ok()) {
+            if file.metadata().unwrap().is_file() {
+                if file.file_name().to_str().unwrap().ends_with("cue") {
+                    split(file);
+                }
+            }
+        }
+    }
+    Ok(())
 }
